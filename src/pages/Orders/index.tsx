f0fb064448873368f9559e70eb0ecd1e923b77ce
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-19 19:39:16 -0300

    Create orders and favorites pages

---
+++ src/pages/Orders/index.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import { Image } from 'react-native';
+
+import api from '../../services/api';
+import formatValue from '../../utils/formatValue';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  FoodsContainer,
+  FoodList,
+  Food,
+  FoodImageContainer,
+  FoodContent,
+  FoodTitle,
+  FoodDescription,
+  FoodPricing,
+} from './styles';
+
+interface Food {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  formattedValue: string;
+  thumbnail_url: string;
+}
+
+const Orders: React.FC = () => {
+  const [orders, setOrders] = useState<Food[]>([]);
+
+  useEffect(() => {
+    async function loadOrders(): Promise<void> {
+      const { data } = await api.get<Food[]>('orders');
+      setOrders(
+        data.map(food => ({
+          ...food,
+          formattedValue: formatValue(food.price),
+        })),
+      );
+    }
+
+    loadOrders();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTitle>Meus pedidos</HeaderTitle>
+      </Header>
+
+      <FoodsContainer>
+        <FoodList
+          data={orders}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Food key={item.id} activeOpacity={0.6}>
+              <FoodImageContainer>
+                <Image
+                  style={{ width: 88, height: 88 }}
+                  source={{ uri: item.thumbnail_url }}
+                />
+              </FoodImageContainer>
+              <FoodContent>
+                <FoodTitle>{item.name}</FoodTitle>
+                <FoodDescription>{item.description}</FoodDescription>
+                <FoodPricing>{item.formattedValue}</FoodPricing>
+              </FoodContent>
+            </Food>
+          )}
+        />
+      </FoodsContainer>
+    </Container>
+  );
+};
+
+export default Orders;
