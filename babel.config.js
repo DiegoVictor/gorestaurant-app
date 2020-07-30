8226b0d5b75e3d2726eb590cb35fdb7d55b2a5af
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-30 18:44:22 -0300

    Configure dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'dotenv-import',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: false,
+      },
+    ],
+  ],
 };
