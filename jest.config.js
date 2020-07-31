890c0a6df550383d7a9c77a5d5d630be03bcac26
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-07-31 13:03:14 -0300

    Configurate jest

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,11 @@ module.exports = {
   setupFiles: ['./jest.setup.js'],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/__mocks__/logo-header.png',
+      '<rootDir>/mocks/logo-header.png',
   },
+  collectCoverage: true,
+  coverageDirectory: 'tests/coverage',
+  coverageReporters: ['text', 'lcov'],
+  testMatch: ['**/tests/**/*.spec.tsx'],
+  roots: ['<rootDir>/src/', '<rootDir>/tests/', '<rootDir>/mocks/'],
 };
