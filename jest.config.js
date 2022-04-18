d05a487da43e48705f62c606b705a84d14abf081
Diego Victor <diegovictorgonzaga@gmail.com>
2022-04-18 10:36:49 -0300

    Update jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,20 @@
 module.exports = {
-  preset: '@testing-library/react-native',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  collectCoverage: true,
+  coverageDirectory: 'tests/coverage',
+  coverageReporters: ['text', 'lcov'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['./jest.setup.js'],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/mocks/logo-header.png',
   },
-  collectCoverage: true,
-  coverageDirectory: 'tests/coverage',
-  coverageReporters: ['text', 'lcov'],
-  testMatch: ['**/tests/**/*.spec.tsx'],
+  preset: '@testing-library/react-native',
   roots: ['<rootDir>/src/', '<rootDir>/tests/', '<rootDir>/mocks/'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|react-native-vector-icons)/)',
+  ],
 };
