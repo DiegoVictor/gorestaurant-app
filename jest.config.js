a2c461244da51a8490a59005c168935451899594
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-10-22 13:35:41 -0300

    Refactor jest.config.js to remove unused coverage settings and simplify moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,7 @@
 module.exports = {
-  collectCoverage: true,
-  coverageDirectory: 'tests/coverage',
-  coverageReporters: ['text', 'lcov'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
-    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/mocks/logo-header.png',
+    '\\.(ttf)$': '<rootDir>/tests/mocks/file-mock.js',
   },
-  preset: '@testing-library/react-native',
-  roots: ['<rootDir>/src/', '<rootDir>/tests/', '<rootDir>/mocks/'],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.tsx?$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(@react-native|react-native|react-native-vector-icons)/)',
-  ],
+  preset: 'react-native',
+  transformIgnorePatterns: ['node_modules/(?!(react-native|@react-native)/)'],
 };
