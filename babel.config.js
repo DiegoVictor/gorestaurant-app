6508061f5a8fc5ea068581c9519b58b227a71223
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-10-24 20:04:15 -0300

    Refactor Babel and TypeScript configurations; update API service to use process.env for baseURL

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,7 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'dotenv-import',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blacklist: null,
-        whitelist: null,
-        safe: false,
-        allowUndefined: false,
-      },
-    ],
+  presets: [
+    'module:@react-native/babel-preset',
+    ['@babel/preset-env', { targets: { node: 'current' } }],
   ],
+  plugins: [],
 };
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
-// eslint-disable-next-line import/no-unresolved
-import { API_URL } from '@env';
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: process.env.API_URL,
 });
 
 export default api;
