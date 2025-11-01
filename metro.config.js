026a33948e3f6d7748aabfa6bfbf54699aba2692
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-11-01 20:36:01 -0300

    Refactor Metro configuration to use default settings and simplify the config structure

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,5 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-};
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
