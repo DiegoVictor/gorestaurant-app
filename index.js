c4231205463868a7e2c88477ab759fb950b1e8d9
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-11-07 13:40:35 -0300

    Refactor index.js to use registerRootComponent from Expo and clean up imports

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import 'intl';
-import 'intl/locale-data/jsonp/pt-BR';
-import { AppRegistry } from 'react-native';
+import { registerRootComponent } from "expo";
 
-import App from './src';
-import { name as appName } from './app.json';
+import App from "./src/App";
 
-AppRegistry.registerComponent(appName, () => App);
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
