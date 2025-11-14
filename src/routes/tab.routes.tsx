9a52bd4a3121e3e62fb5ad851c014c65ece0f6ca
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-11-14 10:25:42 -0300

    Refactor imports in tab.routes.tsx for consistency and clarity

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Feather } from '@react-native-vector-icons/feather';
-
-import Dashboard from '../pages/Dashboard';
-import Favorites from '../pages/Favorites';
-import Orders from '../pages/Orders';
+import { Dashboard } from '../pages/Dashboard';
+import { Favorites } from '../pages/Favorites';
+import { Orders } from '../pages/Orders';
 
 export type BottomTabParamList = {
   Dashboard: undefined;
