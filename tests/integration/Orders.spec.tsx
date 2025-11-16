5288283b0c7961b38e95199b87e41db169fc1377
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-11-16 09:20:26 -0300

    Update orders screen tests

--- tests/integration/Orders.spec.tsx
+++ tests/integration/Orders.spec.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 import AxiosMock from 'axios-mock-adapter';
-
-import api from '../../src/services/api';
-import Orders from '../../src/pages/Orders';
+import { api } from '../../src/services/api';
+import { Orders } from '../../src/pages/Orders';
 import factory from '../utils/factory';
 
 interface Order {
@@ -37,13 +36,13 @@ const apiMock = new AxiosMock(api);
 describe('Orders', () => {
   it('should be able to list the orders', async () => {
     const orders = await factory.attrsMany<Order>('Order', 2);
+    const [order] = orders;
 
     apiMock.onGet('/orders').reply(200, orders);
 
-    const { getByText } = render(<Orders />);
+    const { getByText, findByText } = render(<Orders />);
 
-    const [order] = orders;
-    await waitFor(() => getByText(order.name));
+    await findByText(order.name);
 
     orders.forEach(({ name, description }) => {
       expect(getByText(name)).toBeTruthy();
